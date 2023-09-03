b1855584f7356aaec1754eaaa14bbf3447f75bf0
Yandro Portal <suarez.portal@gmail.com>
2023-09-02 23:30:16 -0500

    Update secret_key.py

--- secret_key.py
+++ secret_key.py
@@ -1,17 +1,3 @@
-"""
-create virtual environment and install packages
-
-python -m venv email_jwt_env
-source email_jwt_env/bin/activate  # On Windows: email_jwt_env\Scripts\activate
-pip install pyjwt smtplib
-
-
-to run program:
-
-python email_sender.py
-"""
-
-
 SECRET_KEY = "YOUR_SECRET_KEY"  # Make sure this is kept secret
 EMAIL_ADDRESS = "your_email@example.com"
 EMAIL_PASSWORD = "your_password"
